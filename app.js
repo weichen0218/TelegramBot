42d9141a4a7811719dd6a849a1b953e52d927db7
weichen0218 <116803419+weichen0218@users.noreply.github.com>
2022-10-27 18:26:06 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+const telegramBot = require("node-telegram-bot-api");
+const Sentiment = require("sentiment");
+const sentiment = new Sentiment();
+const token = "5709250277:AAEOqDocCZRjfGbME1Ud3lk2-vNJcOu9toQ";
+const api = new telegramBot(token, {
+  polling: true,
+});
+
+api.onText(/\/help/, function (msg, match) {
+  const fromId = msg.from.id;
+  api.sendMessage(
+    fromId,
+    "I can help you in getting the sentiments of ant text you send to me."
+  );
+});
+
+api.onText(/\/start/, function (msg, match) {
+  const fromId = msg.from.id;
+  api.sendMessage(
+    fromId,
+    "They call me Pedro." +
+      "I can help you in getting the sentiments of any text you send to me." +
+      "To help you I just have few commands. \n/help\n/start\n/sentiments"
+  );
+});
+
+const opts = {
+  reply_markup: JSON.stringify({
+    force_reply: true,
+  }),
+};
+
+api.onText(/\/sentiments/, function (msg, match) {
+  const fromId = msg.from.id;
+  api.sendMessage(
+      fromId,
+      "Alright! So you need sentiments of a text from me.I can help you in that. Just send me the text.",
+      opts
+    )
+    .then(function (sended) {
+      const chatId = sended.chat.id;
+      const messageId = sended.message_id;
+      api.onReplyToMessage(chatId, messageId, function (message) {
+        const result = sentiment.analyze(message.text);
+        api.sendMessage(
+          fromId,
+          "So sentiments for you text are, Score:" +
+            result.score +
+            " Comparative:" +
+            result.comparative
+        );
+      });
+    });
+});
+
+console.log("Pedro has started. Start conversations in your Telegram.");
